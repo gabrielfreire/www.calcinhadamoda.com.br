318b817741058c974414fd92981fb6b92ef135b3
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-10-30 17:27:05 -0200

    add background principal

--- index.php
+++ index.php
@@ -9,10 +9,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/calcinhadamoda.css" rel="stylesheet" type="text/css"/>
         
     </head>
     <body>
-        <div>
+        <div id="top">            
+            <h1>Calcinha da Moda</h1>
+            <p>PARA A MULHER QUE SE VALORIZA</p>
+        </div>
+        
+        <div id="content_1">
         
         
         
