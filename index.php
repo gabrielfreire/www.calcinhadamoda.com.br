be528ba147133c0da0d584e851d035d89bd4602e
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-10-30 17:04:51 -0200

    add arquivos css

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<??>
+<!DOCTYPE html>
+<!--
+
+-->
+<html>
+    <head>
+        <title>..::CALCINHA DA MODA::..</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>
+        <div>
+        
+        
+        
+        </div>
+    </body>
+</html>
