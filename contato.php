cc1fbb036213af2b93edb4410a5c18962d4aeb63
Gabriel Freire <gabrielfreire14@hotmail.com>
2014-10-31 18:45:24 -0200

    terminei pág de contato

---
+++ contato.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--
+
+-->
+<html>
+    <head>
+        <title>..::CALCINHA DA MODA::..</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/calcinhadamoda.css" rel="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>
+        <div id="top">            
+            <h1>Calcinha da Moda</h1>
+            <p>PARA A MULHER QUE SE VALORIZA</p>
+        </div>
+        
+        <div id="content_1">
+            <?php include "./includes/menu.html" ?>
+        
+            
+            <div id="contato">
+                <h2>Formulário de contato</h2>
+                
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d116976.85869013754!2d-46.783749150000006!3d-23.621296700000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1414786578034" frameborder="0"></iframe>
+                <form action="#" method="post">                
+                    <input type="text" name="nome" placeholder="Nome*" />
+                    <input type="text" name="email" placeholder="Email*" />
+                    <input type="text" name="telefone" />
+                    <textarea name="mensagem" placeholder="Mensagem*"></textarea>
+                    <input type="submit" name="enviar" value="ENVIAR" />
+                </form>
+         
+                <blockquote>                    
+                    <strong>9870 Regis Bittencourt,<br/> Taboão da Serra - SP</strong>
+                    <br/><br/>
+                    <p>Telefone:  +1 800 603 6035</p>
+                    <p>FAX:        +1 800 889 9898</p>
+                    <p>E-mail: 
+                        <a href="mailto:contato@calcinhadamoda.com.br">contato@calcinhadamoda.com.br</a>
+                    </p>
+                </blockquote>
+                
+            </div>
+        </div>
+        
+        
+        <?php include "./includes/rodape.html" ?>
+        
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.js"></script>
+        <script type="text/javascript">
+            $('#upArrow').on("click", function (e) {
+                e.preventDefault();
+                $('html, body').animate({scrollTop: '0px'}, 1000);
+            });
+            
+        </script>
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--
+
+-->
+<html>
+    <head>
+        <title>..::CALCINHA DA MODA::..</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="css/normalize.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/calcinhadamoda.css" rel="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>
+        <div id="top">            
+            <h1>Calcinha da Moda</h1>
+            <p>PARA A MULHER QUE SE VALORIZA</p>
+            <img src="images/arrow.png" alt=""/>
+        </div>
+        
+        <div id="content_1">
+            <?php include "./includes/menu.html" ?>
+        
+            
+            <div id="info">
+                <h1>Bem vindo!</h1>
+                <img src="images/index_img1.jpg" alt="" class="index_img"/>
+                <img src="images/index_img2.jpg" alt="" class="index_img"/>
+                <img src="images/index_img3.jpg" alt="" class="index_img"/>
+
+                <p>
+                    Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja. 
+                </p>
+
+                <p>
+                    Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, 
+                    Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, Venha conhecer a nossa loja, 
+                    Venha conhecer a nossa loja, Venha conhecer a nossa loja.
+                </p>
+            </div>
+        </div>
+        
+        
+        <div id="content_2">
+            <h2>Clientes</h2>
+            <img src="images/clientes/logo-1.png" alt="" />
+            <img src="images/clientes/logo-2.png" alt="" />
+            <img src="images/clientes/logo-3.png" alt="" />
+            <img src="images/clientes/logo-4.png" alt="" />
+            <img src="images/clientes/logo-5.png" alt="" />
+            <img src="images/clientes/logo-6.png" alt="" />
+            
+        </div>
+        
+        <?php include "./includes/rodape.html" ?>
+        
+        <!-- Javascript do site -->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.1.js"></script>
+        <script type="text/javascript">
+            $('#upArrow').on("click", function (e) {
+                e.preventDefault();
+                $('html, body').animate({scrollTop: '0px'}, 1000);
+            });
+
+        </script>
+    </body>
+
+</html>
